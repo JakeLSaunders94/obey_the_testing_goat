c3911af31a5d3be47769ed973598e9c674f7e562
productiononline <jake.saunders@productiononline.org>
2020-11-24 14:22:55 +0000

    Refactored URLS to seperate lists from base project

--- lists/urls.py
+++ lists/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 import lists.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', lists.views.home_page, name='home_page'),
-    path('lists/', include('lists.urls'))
+    path('current/<list_id>', lists.views.view_list, name='view_list'),
+    path('new', lists.views.new_list, name='new_list'),
+    path('current/<list_id>/add_item', lists.views.add_item, name='add_item')
 ]
